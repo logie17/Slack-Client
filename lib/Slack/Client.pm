58913e32356741eca27e83d846a30d569b13319d
Logan Bell <loganbell@gmail.com>
2014-10-03 10:18:48 -0700

    It works!

--- lib/Slack/Client.pm
+++ lib/Slack/Client.pm
@@ -1,34 +1,34 @@
-use strict;
-use warnings;
 package Slack::Client;
 use Moo;
 use Mojo::UserAgent;
 
 our $VERSION = 0.001;
 
-has api_key => (is => 'rw', required => 1);
-has domain => (is => 'rw', required => 1);
-has agent => (is => 'rw', lazy => 1);
+has auth_token => (is => 'rw', required => 1);
+has agent => (is => 'rw', lazy => 1, builder => '_build_agent');
+has base_url => (is => 'ro', required => 1, default => sub { 'https://slack.com' });
 
 sub _build_agent { 
     my $self = shift;
-    my $agent = Mojo::UserAgent->new 
+    my $agent = Mojo::UserAgent->new;
     $agent->on(start => sub {
             my ($ua, $tx) = @_;
-            $tx->req->headers->header('Authoriziation' => 'Bearer ' . $self->api_key);
+            $tx->req->headers->header('Authoriziation' => 'Bearer ' . $self->auth_token);
     });
+    return $agent;
 }
 
 sub post_message {
-    my ($self) = @_;
+    my ($self, %params) = @_;
 
-    $self->agent->post(
-        'https://slack.com/api/chat.postMessage' =>
-        json => {
-            channel => '#',
-            text => "",
-            username => "",
-        };
+    my $res = $self->agent->post(
+        $self->base_url . '/api/chat.postMessage' =>
+        form => {
+            channel => '#' . $params{channel},
+            text => $params{text},
+            username => $params{username},
+	    token => $self->auth_token,
+        }
     );
 }
 
