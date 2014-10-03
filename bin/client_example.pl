b6191d78be64ec27f4124a42bf64771fa07e7b2f
Logan Bell <loganbell@gmail.com>
2014-10-03 10:22:16 -0700

    Make messages easier

--- bin/client_example.pl
+++ bin/client_example.pl
@@ -10,9 +10,10 @@ my $auth_token = $ENV{SLACK_AUTH_TOKEN};
 GetOptions(
     "auth-token=s" => \$auth_token,
     "channel=s" => \(my $channel),
-    "text=s" => \(my $text),
     "username=s" => \(my $username),
 );
+
+my $text = join " ", @ARGV;
 
 if ($text && $channel && $username) {
     Slack::Client->new(auth_token => $auth_token)->post_message(
